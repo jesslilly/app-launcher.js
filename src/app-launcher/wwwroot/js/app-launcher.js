d8d5093cc4b2a9ce745f817eeb48c2cd0b6e534b
jlilly <jlilly@veic.org>
2016-09-13 07:52:38 -0400

    Prototype grid

--- src/app-launcher/wwwroot/js/app-launcher.js
+++ src/app-launcher/wwwroot/js/app-launcher.js
@@ -1,39 +1,44 @@
-﻿(function (document, window) {
+﻿document.appLauncher = (function (document, window) {
 
     console.log("app-launcher loaded.");
 
-    /**
-     * Use XHR to get the dom of the app list page.
-     * @param {} callback 
-     * @returns {} 
-     */
-    function getAppListDom(callback) {
-        var xhr = new XMLHttpRequest();
-        xhr.onload = function () {
-            console.log(this.responseXML.title);
-            callback(this.responseXML);
-        }
-        xhr.open("GET", "/Home/AppList");
-        xhr.responseType = "document";
-        xhr.send();
+    function init(containerElement, apps) {
+
+        var table = document.createElement("table");
+        table.className = "app-launcher-apps";
+        var tr = document.createElement("tr");
+        table.appendChild(tr);
+
+        var columns = 4; // todo option.
+
+        apps.forEach(function(app, index) {
+
+            if (index % columns === 0) {
+                tr = document.createElement("tr");
+                table.appendChild(tr);
+            }
+
+            var td = document.createElement("td");
+            tr.appendChild(td);
+
+            var a = document.createElement("a");
+            a.href = app.href;
+            var img = document.createElement("img");
+            img.src = "broken";
+            img.height = "100";
+            img.width = "100";
+            a.appendChild(img);
+            var linkText = document.createTextNode(app.name);
+            a.appendChild(linkText);
+            td.appendChild(a);
+
+        });
+
+        containerElement.appendChild(table);
     }
 
-    /**
-     * Using a dom, find the app list table and convert into an object.
-     */
-    getAppListDom(function (dom) {
-        console.log(dom.title);
-
-        var rows = dom.querySelector("table").querySelectorAll("tr");
-        var apps = [];
-
-        // start at 1 since the first row is a header.
-        for (var index = 1; index < rows.length; index++) {
-            var name = rows[index].cells[0].innerText.trim();
-            var href = rows[index].cells[1].querySelector("a").href;
-            apps.push({ "name": name, "href": href });
-        }
-        console.log(JSON.stringify(apps));
-    });
+    return {
+        "init": init
+    };
 
 })(document, window);
--- src/app-launcher/wwwroot/js/site.js
+++ src/app-launcher/wwwroot/js/site.js
@@ -1,1 +1,42 @@
-﻿// Write your Javascript code.
+﻿(function (document, window) {
+
+    console.log("site.js loaded.");
+
+    /**
+     * Use XHR to get the dom of the app list page.
+     * @param {} callback 
+     * @returns {} 
+     */
+    function getAppListDom(callback) {
+        var xhr = new XMLHttpRequest();
+        xhr.onload = function () {
+            console.log(this.responseXML.title);
+            callback(this.responseXML);
+        }
+        xhr.open("GET", "/Home/AppList");
+        xhr.responseType = "document";
+        xhr.send();
+    }
+
+    /**
+     * Using a dom, find the app list table and convert into an object.
+     */
+    getAppListDom(function (dom) {
+        console.log(dom.title);
+
+        var rows = dom.querySelector("table").querySelectorAll("tr");
+        var apps = [];
+
+        // start at 1 since the first row is a header.
+        for (var index = 1; index < rows.length; index++) {
+            var name = rows[index].cells[0].innerText.trim();
+            var href = rows[index].cells[1].querySelector("a").href;
+            apps.push({ "name": name, "href": href });
+        }
+        console.log(JSON.stringify(apps));
+
+        var container = document.querySelector("#apps");
+        document.appLauncher.init(container, apps);
+    });
+
+})(document, window);
