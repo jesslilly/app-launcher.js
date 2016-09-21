cd86819caa2754559c723fb7c67451e2525b2705
jlilly <jlilly@veic.org>
2016-09-21 09:10:13 -0400

    Just a comment about CORS.

--- src/app-launcher/wwwroot/js/site.js
+++ src/app-launcher/wwwroot/js/site.js
@@ -2,7 +2,10 @@
 
     console.log("site.js loaded.");
 
+    // To prevent CORS issues: Use relative URL, protocol agnostic (beginning with //), 
+    // or "direct" url without redirects.
     var uri = "/Home/AppList";
+
     document.appLauncher.scrapeTable(uri, function(appList) {
         var container = document.querySelector("#apps");
         document.appLauncher.init(container, appList);
