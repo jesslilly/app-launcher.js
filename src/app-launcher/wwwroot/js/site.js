0399a7b41059c1b8a23ba38264246c4b7acd6bb0
jlilly <jlilly@veic.org>
2016-09-22 22:42:00 -0400

    wire up environment-select

--- src/app-launcher/wwwroot/js/site.js
+++ src/app-launcher/wwwroot/js/site.js
@@ -4,11 +4,20 @@
 
     // To prevent CORS issues: Use relative URL, protocol agnostic (beginning with //), 
     // or "direct" url without redirects.
-    var uri = "/Home/AppList";
 
-    document.appLauncher.scrapeTable(uri, 2, function(appList) {
-        var container = document.querySelector("#apps");
-        document.appLauncher.init(container, appList);
+    function createAppLauncher(columnNumber) {
+        var uri = "/Home/AppList";
+        document.appLauncher.scrapeTable(uri, columnNumber, function (appList) {
+            var container = document.querySelector("#apps");
+            document.appLauncher.init(container, appList);
+        });
+    }
+    var select = document.querySelector("#environment-select");
+    select.addEventListener("change", function (event) {
+        var columnNumber = parseInt(this.value, 10);
+        console.log("createAppLauncher for column " + columnNumber);
+        createAppLauncher(columnNumber);
     });
+    createAppLauncher(3);
 
 })(document, window);
